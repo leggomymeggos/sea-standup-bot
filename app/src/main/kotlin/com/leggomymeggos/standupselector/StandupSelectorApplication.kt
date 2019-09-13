f1f2aea7c53db7089bfe657abdad8acdaae5e093
Megan Gaebler <megan.gaebler@gmail.com>
2019-09-12 20:59:10 -0700

    Initial commit

---
+++ app/src/main/kotlin/com/leggomymeggos/standupselector/StandupSelectorApplication.kt
@@ -0,0 +1,11 @@
+package com.leggomymeggos.standupselector
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StandupSelectorApplication
+
+fun main(args: Array<String>) {
+    runApplication<StandupSelectorApplication>(*args)
+}
---
+++ ganonleap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+unicornleap -u ~/workspace/kotlin-standup-selector/test_images/ganon.png -k ~/workspace/kotlin-standup-selector/test_images/rupee.png $@
